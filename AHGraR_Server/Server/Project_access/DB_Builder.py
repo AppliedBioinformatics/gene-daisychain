9c2a011eadfaf17acd43f9820c658d074a389775
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-04 13:27:02 +0000

    Continued work on project DB build
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@365 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/DB_Builder.py
+++ AHGraR_Server/Server/Project_access/DB_Builder.py
@@ -109,9 +109,10 @@ class DBBuilder:
         for species in file_dict:
             # Identify the GFF/CSV annotation file in the file_dict list by sorting the list alphabetically
             # gff < prot and csv < prot
-            fasta_file = sorted(file_dict[species], key=lambda x: x[1])[1]
-            print(fasta_file)
-
+            prot_fasta_file = sorted(file_dict[species], key=lambda x: x[1])[1][0]
+            print("Parsing: "+prot_fasta_file)
+            fasta_parser.parse_fasta(prot_fasta_file)
+        fasta_parser.close_combined_fasta()
 
 
 
