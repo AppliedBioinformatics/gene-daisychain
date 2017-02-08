402ed44c35e79cef3e4d9f2745abb0801d09e065
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-08 18:57:28 +0000

    Continued work on Blastp Homology search
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@387 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/DB_Builder.py
+++ AHGraR_Server/Server/Project_access/DB_Builder.py
@@ -186,8 +186,8 @@ class DBBuilder:
         subprocess.run([os.path.join("Projects", str(proj_id), "proj_graph_db", "bin", "neo4j-import"),
                                      "--into", os.path.join("Projects", str(proj_id), "db_folder"), "--id-type",
                                      "string", "--nodes:Gene gene_nodes.csv",
-                                     "--relationships:5_NB gene_5nb.csv", "--relationships:3_NB gene_3nb.csv",
-                                     "--relationships:HOMOLOG gene_hmlg.csv"], check=True)
+                                     "--relationships:5_NB", "gene_5nb.csv", "--relationships:3_NB", "gene_3nb.csv",
+                                     "--relationships:HOMOLOG", "gene_hmlg.csv"], check=True)
 
         print("Finished")
 
