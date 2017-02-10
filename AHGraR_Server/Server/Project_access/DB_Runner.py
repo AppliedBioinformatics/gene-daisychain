bd70ab2306320c14653818dc1f7787da14ed9071
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-10 06:50:42 +0000

    Added functionality to start/stop/restart/status a project-specific graph db
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@406 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/DB_Runner.py
+++ AHGraR_Server/Server/Project_access/DB_Runner.py
@@ -16,13 +16,14 @@ class DBRunner:
         # Check for correct syntax: Proj_ID + command
         if len(user_request) != 2 or not user_request[0].isdigit():
             self.send_data("-5")
+        proj_id = user_request[0]
         # Define path to neo4j binary
         neo4j_binary = os.path.join("Projects", str(user_request[0]), "proj_graph_db", "bin", "neo4j")
         # Start db?
         if user_request[1] == "START":
-            self.start(neo4j_binary)
+            self.start(neo4j_binary, proj_id)
         elif user_request[1] == "STOP":
-            self.stop(neo4j_binary)
+            self.stop(neo4j_binary, proj_id)
         elif user_request[1] == "STATUS":
             self.get_status(neo4j_binary)
         else:
@@ -30,9 +31,11 @@ class DBRunner:
 
     def get_status(self,neo4j_binary):
         # Retrieve status (running/not running) from neo4j instance
+        # returncode 0: DB is running
+        # returncode 3: DB not running
         try:
             status = subprocess.run([neo4j_binary, "status"], stdout=subprocess.PIPE)
-        except subprocess.CalledProcessError as err:
+        except subprocess.CalledProcessError:
             self.send_data("Unknown")
             return
         if status.returncode == 0:
@@ -42,18 +45,28 @@ class DBRunner:
         else:
             self.send_data("Unknown")
 
-    def start(self,neo4j_binary):
+    def start(self,neo4j_binary, proj_id):
         # Start up the project neo4j database
         self.send_data("Starting Project DB")
-        stdout = subprocess.run([neo4j_binary, "start"], stdout=subprocess.PIPE)
-        print(stdout)
+        try:
+            stdout = subprocess.run([neo4j_binary, "start"], stdout=subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            return
+        # Only change project status if previous subprocess was successfull
+        if stdout.returncode == 0:
+            self.set_project_status(proj_id, "DB_RUNNING")
 
 
-    def stop(self,neo4j_binary):
+    def stop(self,neo4j_binary, proj_id):
         # Start up the project neo4j database
         self.send_data("Stopping Project DB")
-        stdout = subprocess.run([neo4j_binary, "stop"])
-        print(stdout)
+        try:
+            stdout = subprocess.run([neo4j_binary, "stop"])
+        except subprocess.CalledProcessError:
+            return
+        # Only change project status if previous subprocess was successfull
+        if stdout.returncode == 0:
+            self.set_project_status(proj_id, "DB_STOPPED")
 
 
     # Change project status
