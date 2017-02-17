d66ce6d4ec33def6c55a19c0293bdef495cb1498
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-17 08:50:57 +0000

    Started work on project db queries
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@458 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -86,8 +86,8 @@ class QueryManagement:
             for record in hits:
                 hit_elements += [str(record["gene"][item]) for item in ["geneId","species", " chromosome", "contig_name", "start",
                                                          "stop", "gene_name"]]
-                hit_elements[-1] += "\n"
-        self.send_data("\t".join(hit_elements))
+            print(hit_elements)
+        self.send_data("Coming soon")
 
 
 
