85cce06597764f273056f4d4ebb7c3dc38c26379
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-17 07:37:37 +0000

    Started work on project db queries
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@437 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -81,7 +81,7 @@ class QueryManagement:
                                        "AND LOWER(gene.gene_name) CONTAINS {query_name} RETURN(gene)",
                                        {"query_species":query_species, "query_name": query_name})
             for record in hits:
-                print(record)
+                print(record.properties)
         self.send_data("Working on it")
 
 
