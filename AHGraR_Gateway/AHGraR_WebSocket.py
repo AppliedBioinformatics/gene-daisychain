821f0000911c51c7d91f2a860ea7949ebebc7ebc
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-22 11:28:43 +0000

    Started work on AHGraR webserver
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@555 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Gateway/AHGraR_WebSocket.py
+++ AHGraR_Gateway/AHGraR_WebSocket.py
@@ -1,64 +1,15 @@
-# Websocket to facilitate communication between AHGraR-web and AHGraR-server
 import asyncio
 import websockets
-import configparser
-import socket
 
-async def handle(websocket, path):
-    # Receive request from web application
-    web_request = await websocket.recv()
-    # Forward request to AHGraR gateway
-    #connection = socket.create_connection(('146.118.99.190', gateway_port))
-    #send_data(connection, web_request)
-    #gateway_reply = receive_data(connection)
-    websocket.send("Gotcha")
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
 
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+    print("> {}".format(greeting))
 
+start_server = websockets.serve(hello, 'localhost', 8765)
 
-def send_data(connection, reply):
-    # Add length of message to header
-    message = str(len(reply)) + "|" + reply
-    connection.sendall(message.encode())
-
-    # Receive data coming in from server or user
-
-
-def receive_data(self, connection):
-    # First, determine the length of the message
-    # The message has a header containing the length
-    # of the actual message:
-    # e.g. 123|Data bla bla
-    # First, receive data bytewise until the "|" is detected
-    msg_header = ""
-    while True:
-        incoming_data = connection.recv(1).decode()
-        if incoming_data == "|":
-            break
-        else:
-            msg_header += incoming_data
-    # Store length of the actual message
-    msg_length = int(msg_header)
-    # Start to build up the actual message
-    msg = ""
-    # Receive chunks of data until the length of the received message equals the expected length
-    while msg_length > 0:
-        # Receive a max. of 1024 bytes
-        rcv_length = 1024 if msg_length >= 1024 else msg_length
-        msg_chunk = connection.recv(rcv_length).decode()
-        msg += msg_chunk
-        # Subtract the actual length of the received message from the overall message length
-        msg_length -= len(msg_chunk)
-    return (msg)
-# Read AHGraR config file
-ahgrar_config = configparser.ConfigParser()
-try:
-    ahgrar_config.read('AHGraR_config.txt')
-except OSError:
-    print("Config file not found. Exiting.")
-    exit(3)
-# Connect to gateway, running on the same server (i.e. localhost)
-gateway_port = int(ahgrar_config['AHGraR_Server']['port'])
-start_server = websockets.serve(handle, 'localhost', 7000)
-class_var = "ABC"
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
--- AHGraR_Gateway/WebSocket_Client_test.py
+++ AHGraR_Gateway/WebSocket_Client_test.py
@@ -2,11 +2,13 @@ import asyncio
 import websockets
 
 async def hello():
-    async with websockets.connect('ws://localhost:7000') as websocket:
-        cmd = input("Command?")
-        await websocket.send(cmd)
-        reply = await websocket.recv()
-        print("< {}".format(reply))
+    async with websockets.connect('ws://localhost:8765') as websocket:
+
+        name = input("What's your name? ")
+        await websocket.send(name)
+        print("> {}".format(name))
+
+        greeting = await websocket.recv()
+        print("< {}".format(greeting))
 
 asyncio.get_event_loop().run_until_complete(hello())
-
