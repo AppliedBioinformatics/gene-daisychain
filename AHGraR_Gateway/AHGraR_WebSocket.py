9a778b6a95d7990b8b7535975b59e4ce1a94936f
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-22 11:45:06 +0000

    Started work on AHGraR webserver
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@556 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Gateway/AHGraR_WebSocket.py
+++ AHGraR_Gateway/AHGraR_WebSocket.py
@@ -1,15 +1,25 @@
 import asyncio
 import websockets
+import configparser
+import socket
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print("< {}".format(name))
-
-    greeting = "Hello {}!".format(name)
+async def handle(websocket, path):
+    web_request = await websocket.recv()
+    print("< {}".format(web_request))
+    ahgrar_config = configparser.ConfigParser()
+    try:
+        ahgrar_config.read('AHGraR_config.txt')
+    except OSError:
+        print("Config file not found. Exiting.")
+        exit(3)
+    connection = socket.create_connection(
+        (ahgrar_config['AHGraR_Gateway']['ip'], ahgrar_config['AHGraR_Gateway']['port']))
+    connection.close()
+    greeting = "Hello {}!".format(web_request)
     await websocket.send(greeting)
     print("> {}".format(greeting))
 
-start_server = websockets.serve(hello, 'localhost', 8765)
+start_server = websockets.serve(handle, 'localhost', 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
