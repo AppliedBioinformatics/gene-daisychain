eec1d3e6b4db8947837d9e6104f9bcda1399e499
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-22 07:37:06 +0000

    Retrieve relationships of a defined type for a distinct node
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@550 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -91,7 +91,7 @@ class QueryManagement:
         protein_gene_node_rel = []
         # Search for a "5_NB" pr "3_NB" relationship between gene node and gene node
         if relationship_type in ["5_NB", "3_NB"]and node_type == "Gene":
-            query_hits = project_db_conn.run("MATCH(gene:Gene)-[rel:'"+relationship_type+"']->(targetGene:Gene) "
+            query_hits = project_db_conn.run("MATCH(gene:Gene)-[rel:`"+relationship_type+"`]->(targetGene:Gene) "
                                              "WHERE gene.geneId = {geneId} RETURN gene, rel, targetGene",
                                              {"geneId": node_id} )
             for record in query_hits:
