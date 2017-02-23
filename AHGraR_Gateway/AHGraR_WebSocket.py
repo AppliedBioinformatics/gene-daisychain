04f9d19a8185acdab528868349f0f7877d10fc69
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-23 05:24:58 +0000

    Started work on AHGraR webserver
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@569 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Gateway/AHGraR_WebSocket.py
+++ AHGraR_Gateway/AHGraR_WebSocket.py
@@ -5,13 +5,6 @@ import socket
 
 async def handle(websocket, path):
     web_request = await websocket.recv()
-    print("< {}".format(web_request))
-    ahgrar_config = configparser.ConfigParser()
-    try:
-        ahgrar_config.read('AHGraR_config.txt')
-    except OSError:
-        print("Config file not found. Exiting.")
-        exit(3)
     connection = socket.create_connection(
         (ahgrar_config['AHGraR_Gateway']['ip'], ahgrar_config['AHGraR_Gateway']['port']))
     reply = "PMINFO"
@@ -23,6 +16,12 @@ async def handle(websocket, path):
     await websocket.send(greeting)
     print("> {}".format(greeting))
 
+ahgrar_config = configparser.ConfigParser()
+try:
+    ahgrar_config.read('AHGraR_config.txt')
+except OSError:
+    print("Config file not found. Exiting.")
+    exit(3)
 start_server = websockets.serve(handle, '0.0.0.0', 7687)
 
 asyncio.get_event_loop().run_until_complete(start_server)
