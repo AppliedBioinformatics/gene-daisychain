c188fb7787ad778055bc8d60ed4c54f2f93d6b5a
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-02-25 06:55:27 +0000

    Debugging web query search
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@646 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -200,6 +200,11 @@ class QueryManagement:
     # Reformat node and edge data to fit the format expected by AHGraR-web
     def send_data_web(self, gene_node_hits, protein_node_hits, gene_node_rel, protein_node_rel,
                       protein_gene_node_rel):
+        print(gene_node_hits)
+        print(protein_node_hits)
+        print(gene_node_rel)
+        print(protein_node_rel)
+        print(protein_gene_node_rel)
         # Transfer gene node and protein node dicts into list structures
         # Sort gene node list by species,chromosome, contig, start
         gene_node_hits = [[item[0]] + item[1] for item in gene_node_hits.items()]
