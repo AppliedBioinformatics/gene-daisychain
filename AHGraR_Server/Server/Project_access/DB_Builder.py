8945b2c7951be07d245e6c76f890b257bf5c29eb
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-09 17:29:27 +0000

    Started work on project build in AHGraR-admin
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@750 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/DB_Builder.py
+++ AHGraR_Server/Server/Project_access/DB_Builder.py
@@ -313,7 +313,7 @@ class DBBuilder:
         #     self.task_mngr.add_task_results(proj_id, task_id, "Correct Annotation: " + str(valid_annotation_mapper)+ " Correct Hierarchy: "+ str(valid_feature_hierarchy))
         #     return
         # Else add them to the main-db
-        self.main_db_conn.run("MATCH(proj:Project)-[:has_files]->(fileMngr:File_Manager) WHERE ID(proj)={proj_id} "
+        res = self.main_db_conn.run("MATCH(proj:Project)-[:has_files]->(fileMngr:File_Manager) WHERE ID(proj)={proj_id} "
                     "MATCH (fileMngr)-[:file]->(file:File) WHERE file.filename = {file_name} "
                     "AND file.filetype = 'gff3' AND file.hidden = 'False' "
                     "SET file.parent_feat = {parent_feat} SET file.sub_features = {sub_features} "
@@ -321,6 +321,8 @@ class DBBuilder:
                     {"proj_id": int(proj_id), "file_name": file_name,
                      "parent_feat": parent_feat, "sub_features": sub_features,
                      "name_attr":name_attr, "desc_attr":desc_attr})
+        for r in res:
+            print(r)
         self.task_mngr.set_task_status(proj_id, task_id, "Added annotation to main-db")
         # Test the parsing of the GFF3 file
         # Make a copy of the gff3-fie, copying only the first 100 lines
