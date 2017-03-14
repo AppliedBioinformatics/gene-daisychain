f5abcc0e397d03287c43c7e1c1655849e61c1b16
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-14 04:20:20 +0000

    Match for all or any keywords in query
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@863 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -365,6 +365,7 @@ class QueryManagement:
         query_contig = str(query_term[1]).lower()
         # Separate keywords by whitespace
         query_keyword = str(query_term[2]).lower().split(" ")
+        match_all_any = "ALL"
 
         # Collect gene node hits and protein node hits
         # Also collect relations between gene nodes, protein nodes
@@ -396,11 +397,11 @@ class QueryManagement:
             #                                   "query_chromosome": query_chromosome, "query_anno": query_keyword}) #ALL(x in ["conserve"] WHERE gene.descr contains x)
         query_hits = project_db_conn.run("MATCH(gene:Gene) WHERE LOWER(gene.species) CONTAINS {query_species} "
                                          "AND LOWER(gene.contig) CONTAINS {query_contig} "
-                                         "AND (ALL(term in {query_keyword} WHERE LOWER(gene.name) CONTAINS term) OR "
-                                         "ALL(term in {query_keyword} WHERE LOWER(gene.descr) CONTAINS term)) "
+                                         "AND ({ALL_ANY}(term in {query_keyword} WHERE LOWER(gene.name) CONTAINS term) OR "
+                                         "{ALL_ANY}(term in {query_keyword} WHERE LOWER(gene.descr) CONTAINS term)) "
                                          "OPTIONAL MATCH (gene)-[rel]->(gene_nb:Gene) RETURN gene,rel,gene_nb",
                                          {"query_species": query_species, "query_keyword": query_keyword,
-                                          "query_contig": query_contig})
+                                          "query_contig": query_contig, "ALL_ANY": match_all_any})
             # The record format lists every node matching the query together with one relationship to another gene node.
             # Multiple relationships are split into separate rows, e.g. row 1 contains the 5` relation of g1 with g2,
             # row 2 contains the 3` relation of g1 with g3. If there is no gene-gene relationship going out from g1
