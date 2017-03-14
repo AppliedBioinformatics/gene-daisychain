c5f18ca1eba905291deb727e8e66a91dfc951bd9
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-14 14:48:52 +0000

    Corrected retrieval of websocket
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@872 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/AHGraR_Server.py
+++ AHGraR_Server/AHGraR_Server.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
     admin_socket_thread.start()
     # Fire up websocket for query connection
     query_websocket = AHGraRWebSocket().get_websocket(server_address,server_query_port)
-    asyncio.get_event_loop().run_until_complete(query_websocket)
+    #asyncio.get_event_loop().run_until_complete(query_websocket)
     asyncio.get_event_loop().run_forever()
     print("Listening for admin connections at "+server_address+":"+str(server_admin_port))
     print("Listening for query connections at " + server_address + ":" + str(server_query_port))
