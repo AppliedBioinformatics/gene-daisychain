2877deeb2aa271d28a0b016dd6df4fa818ce9137
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-16 06:37:48 +0000

    Added secondary node relation retrieval to 5 and 3 NB relation query
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@917 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_Server/Server/Project_access/Query_Management.py
+++ AHGraR_Server/Server/Project_access/Query_Management.py
@@ -191,7 +191,10 @@ class QueryManagement:
         # Search for a "5_NB" pr "3_NB" relationship between gene node and gene node
         if relationship_type in ["5_NB", "3_NB"]and node_type == "Gene":
             query_hits = project_db_conn.run("MATCH(gene:Gene)-[rel:`"+relationship_type+"`]->(targetGene:Gene) "
-                                             "WHERE gene.geneId = {geneId} RETURN gene, rel, targetGene",
+                                             "WHERE gene.geneId = {geneId} "
+                                             "OPTIONAL MATCH (targetGene)-[targetGene_rel]->(secondary_node) "
+                                             "RETURN gene, rel, targetGene, targetGene_rel, "
+                                             "secondary_node.geneId, secondary_node.proteinId",
                                              {"geneId": node_id} )
             for record in query_hits:
                 gene_node_hits[record["targetGene"]["geneId"]] = \
@@ -199,10 +202,39 @@ class QueryManagement:
                                                    "start", "stop", "name", "descr", "nt_seq"]]
                 if relationship_type == "5_NB":
                     gene_node_nb_rel.append((record["gene"]["geneId"], "5_NB", record["targetGene"]["geneId"]))
-                    gene_node_nb_rel.append((record["targetGene"]["geneId"], "3_NB", record["gene"]["geneId"]))
+                    #gene_node_nb_rel.append((record["targetGene"]["geneId"], "3_NB", record["gene"]["geneId"]))
                 else:
                     gene_node_nb_rel.append((record["gene"]["geneId"], "3_NB", record["targetGene"]["geneId"]))
-                    gene_node_nb_rel.append((record["targetGene"]["geneId"], "5_NB", record["gene"]["geneId"]))
+                    #gene_node_nb_rel.append((record["targetGene"]["geneId"], "5_NB", record["gene"]["geneId"]))
+                # Now add secondary relations of new nodes to list of relations
+                if record["targetGene_rel"]:
+                    target_gene_rel_type = record["targetGene_rel"].type
+                    if target_gene_rel_type == "5_NB":
+                        gene_node_nb_rel.append(
+                            (record["targetGene"]["geneId"], "5_NB", record["secondary_node.geneId"]))
+                        gene_node_nb_rel.append(
+                            (record["secondary_node.geneId"], "3_NB", record["targetGene"]["geneId"]))
+                    if target_gene_rel_type == "3_NB":
+                        gene_node_nb_rel.append(
+                            (record["targetGene"]["geneId"], "3_NB", record["secondary_node.geneId"]))
+                        gene_node_nb_rel.append(
+                            (record["secondary_node.geneId"], "5_NB", record["targetGene"]["geneId"]))
+                    if target_gene_rel_type == "HOMOLOG":
+                        gene_node_hmlg_rel.append((record["targetGene"]["geneId"],
+                                                   "HOMOLOG",
+                                                   record["targetGene_rel"]["clstr_sens"],
+                                                   record["targetGene_rel"]["perc_match"],
+                                                   record["secondary_node.geneId"]))
+                        gene_node_hmlg_rel.append((record["secondary_node.geneId"],
+                                                   "HOMOLOG",
+                                                   record["targetGene_rel"]["clstr_sens"],
+                                                   record["targetGene_rel"]["perc_match"],
+                                                   record["targetGene"]["geneId"]))
+                    if target_gene_rel_type == "CODING":
+                        gene_protein_coding_rel.append((record["targetGene"]["geneId"],
+                                                        "CODING",
+                                                        record["secondary_node.proteinId"]))
+
         # # Search for a "CODING" relationship between a gene node and a protein node
         # if relationship_type == "CODING" and node_type == "Gene":
         #     query_hits = project_db_conn.run("MATCH(gene:Gene)-[rel:CODING]->(targetProt:Protein) "
