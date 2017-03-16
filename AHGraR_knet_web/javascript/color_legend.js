4a4c07e7618625422b00aad94cae4eb0d793bb68
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-16 23:46:20 +0000

    Added color legend bar
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@976 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/color_legend.js
+++ AHGraR_knet_web/javascript/color_legend.js
@@ -17,7 +17,8 @@ for(var i = 0; i < species_list.length; i++)
                  // Fill select option with data:
                  btn.innerHTML = species_list[i];
                  btn.value = species_list[i];
-                 btn.style.background=color_node(species_list[i], "");
+                 btn.style.border="2px solid #4CAF50"
+                 //btn.style.background=color_node(species_list[i], "");
                  colorLegend.appendChild(btn);
              };
 
