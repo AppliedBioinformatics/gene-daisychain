8b5ec7da0eea00e3a42dab4738b42f60610a9458
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-16 06:21:00 +0000

    Add warning message if no new nodes were retrieved
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@913 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/extend_graph.js
+++ AHGraR_knet_web/javascript/extend_graph.js
@@ -16,7 +16,7 @@ function addPath(node, rel_type)
         new_edge_data = new_graph_data.edges;
         var angle_rotation = (2 * Math.PI)/new_node_data.length;
         var angle = 0;
-        if (new_node_data.len == 0){
+        if (new_node_data.len != 0){
         window.alert("sometext");
         console.log("zero :-(");
         };
