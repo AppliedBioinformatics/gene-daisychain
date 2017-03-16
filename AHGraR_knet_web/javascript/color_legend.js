c76d3af1a9bf14abaa9d3d9168ced974967a9196
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-16 23:24:09 +0000

    Added color legend bar
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@967 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/color_legend.js
+++ AHGraR_knet_web/javascript/color_legend.js
@@ -3,5 +3,8 @@ function update_color_legend()
 //$('#colorLegend span').text("Huhu");
 var colorLegend = document.getElementById('colorLegend');
 var btn = document.createElement('button');
+btn.setAttribute('type', 'button');
+//btn.setAttribute('onclick', functions[i]);
+btn.setAttribute('id', 'button' + "1");
 colorLegend.appendChild(btn);
 }
