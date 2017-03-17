fa483a08f9bfcbab796e48068795c4a44a3496a4
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-17 00:46:30 +0000

    Added color legend bar
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1003 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/node_show_hide.js
+++ AHGraR_knet_web/javascript/node_show_hide.js
@@ -1,8 +1,9 @@
 function show_hide(species, show)
 {
 var cy= $('#cy').cytoscape('get'); // now we have a global reference to `cy`
-for (var ele in cy.elements())
-{
-console.log(ele);
-}
+cy.nodes().forEach(function( node )
+    {
+        console.log(node.data('species'));
+
+    });
 }
