7405a87c259938dd5f3945f0828246200fc2312a
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 13:32:49 +0000

    Export graph data as csv
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1226 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/export_csv.js
+++ AHGraR_knet_web/javascript/export_csv.js
@@ -1,8 +1,9 @@
 // Export graph data in csv format
 // Each row represents one relation, e.g. one homology relation of geneA to geneB
 function export_table(){
+console.log("Export table");
 var cy= $('#cy').cytoscape('get'); // now we have a global reference to `cy`
-cy.edges().forEach(function(edge){
-console.log(edge);
-});
+var nodes = cy.elements('node');
+console.log(nodes);
+
 }
