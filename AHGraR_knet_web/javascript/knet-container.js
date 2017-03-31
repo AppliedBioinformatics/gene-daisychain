9df5723f02085a90f728892e2f102c74802f6ae0
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 09:32:39 +0000

    Added 2nd ctxt menu for proteins
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1188 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/knet-container.js
+++ AHGraR_knet_web/javascript/knet-container.js
@@ -82,7 +82,7 @@ cy.elements('edge').qtip({
 
 
  /** Popup (context) menu: a circular Context Menu for each Node (concept) & Edge (relation) using the 'cxtmenu' jQuery plugin. */
- var contextMenu= {
+ var ctxt_menu_gene= {
     menuRadius: 75, // the radius of the circular menu in pixels
 
     // Use selector: '*' to set this circular Context Menu on all the elements of the core.
@@ -161,8 +161,89 @@ cy.elements('edge').qtip({
     zIndex: 9999 // the z-index of the ui div
  };
 
-cy.cxtmenu(contextMenu); // set Context Menu for all the core elements.
 
+ var ctxt_menu_protein= {
+    menuRadius: 75, // the radius of the circular menu in pixels
+
+    // Use selector: '*' to set this circular Context Menu on all the elements of the core.
+    /** Note: Specify selector: 'node' or 'edge' to restrict the context menu to a specific type of element. e.g,
+     * selector: 'node', // to have context menu only for nodes.
+     * selector: 'edge', // to have context menu only for edges. */
+    selector: 'node[type="Protein"]',
+    commands: [ // an array of commands to list in the menu
+        {
+         content: 'Show Info',
+         select: function() {
+             // Show Item Info Pane.
+             openItemInfoPane();
+
+             // Display Item Info.
+             showItemInfo(this);
+            }
+        },
+
+        {
+         content: 'Delete',
+         select: function() {
+             //this.hide(); // hide the selected 'node' or 'edge' element.
+             console.log(this);
+            cy.remove(this);
+			 // Refresh network legend.
+             updateCyLegend();
+            }
+        },
+        {
+         content: "Show 5'/3'",
+         select: function()
+            {
+                addPath(this, "53NB");
+            }
+        },
+        {
+         content: "Show protein",
+         select: function()
+            {
+                addPath(this, "CODING");
+            }
+        },
+        {
+         content: "Show homologs",
+         select: function()
+            {
+                addPath(this, "HOMOLOG");
+            }
+        },
+
+
+
+        {
+         content: 'Label on/ off',
+         select: function() {
+             if(this.style('text-opacity') === '0') {
+                this.style({'text-opacity': '1'}); // show the concept/ relation Label.
+               }
+               else {
+                this.style({'text-opacity': '0'}); // hide the concept/ relation Label.
+               }
+            }
+        }
+    ],
+    fillColor: 'rgba(239, 0, 0, 0.75)', // the background colour of the menu
+    activeFillColor: 'rgba(92, 194, 237, 0.75)', // the colour used to indicate the selected command
+    activePadding: 2, // 20, // additional size in pixels for the active command
+    indicatorSize: 15, // 24, // the size in pixels of the pointer to the active command
+    separatorWidth: 3, // the empty spacing in pixels between successive commands
+    spotlightPadding: 3, // extra spacing in pixels between the element and the spotlight
+    minSpotlightRadius: 5, // 24, // the minimum radius in pixels of the spotlight
+    maxSpotlightRadius: 10, // 38, // the maximum radius in pixels of the spotlight
+    itemColor: 'white', // the colour of text in the command's content
+    itemTextShadowColor: 'black', // the text shadow colour of the command's content
+    zIndex: 9999 // the z-index of the ui div
+ };
+
+
+cy.cxtmenu(ctxt_menu_gene); // set Context Menu for all the core elements.
+cy.cxtmenu(ctxt_menu_protein); // set Context Menu for all the core elements.
  // Show the popup Info. dialog box.
  $('#infoDialog').click(function() {
    $('#infoDialog').slideToggle(300);
