d04916e0a096812d480d5ca22f265d70d972009c
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 14:12:47 +0000

    Export graph data as csv
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1238 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/export_csv.js
+++ AHGraR_knet_web/javascript/export_csv.js
@@ -14,13 +14,30 @@ var protein_nodes = cy.filter(function(i,ele){
             else {return false;}
             });
 var coding_edges = cy.filter(function(i,ele){
-            if (ele.isEdge() &&  ele.visible())
+            if (ele.isEdge() &&  ele.data('type') == "CODING" && ele.visible())
             {return true;}
             else {return false;}
             });
-
+var nb5_edges = cy.filter(function(i,ele){
+            if (ele.isEdge() &&  ele.data('type') == "5_NB" && ele.visible())
+            {return true;}
+            else {return false;}
+            });
+var nb3_edges = cy.filter(function(i,ele){
+            if (ele.isEdge() &&  ele.data('type') == "3_NB" && ele.visible())
+            {return true;}
+            else {return false;}
+            });
+var homolog_edges = cy.filter(function(i,ele){
+            if (ele.isEdge() &&  ele.data('type') == "HOMOLOG" && ele.visible())
+            {return true;}
+            else {return false;}
+            });
 console.log(gene_nodes);
 console.log(protein_nodes);
 console.log(coding_edges);
+console.log(nb5_edges);
+console.log(nb3_edges);
+console.log(homolog_edges);
 
 }
