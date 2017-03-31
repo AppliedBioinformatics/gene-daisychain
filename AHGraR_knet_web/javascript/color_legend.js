69c6af6ec8eb71143bc43bef25b3990d12843ab8
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 04:25:34 +0000

    Code cleanup for color legend
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1164 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/color_legend.js
+++ AHGraR_knet_web/javascript/color_legend.js
@@ -1,53 +1,47 @@
-//function set_color_legend()
-//{
-//console.log("Retrieve  color legend");
-//buttons = document.getElementById('colorLegend').childNodes;
-//for (var but in buttons)
-//{
-//console.log(buttons[but].species);
-//show_hide(buttons[but].getAttribute('species'), buttons[but].getAttribute('show'));
-//}
-//}
+// Generate an interactive color legend
+// Each assembly has one color assigned
+// Clicking the respective button in the legend shows/hides all nodes of that assembly
+
+// Generate color legend: Generate clickable buttons
 function update_color_legend(species_list)
 {
-
+// Get reference to div
 var colorLegend = document.getElementById('colorLegend');
-console.log("Set color legend");
-// Remove old elements
-while (colorLegend.firstChild) {
-    colorLegend.removeChild(colorLegend.firstChild);
-}
+// Remove all previous buttons
+while (colorLegend.firstChild) {colorLegend.removeChild(colorLegend.firstChild);}
+// Loop through assembly list, generate a button for each assembly
 for(var i = 0; i < species_list.length; i++)
             {
-                 // Create a new select option
+                 // Create a new button option and set its attributes
                  var btn = document.createElement("button");
                  btn.setAttribute('type', 'button');
+                 // Default: Show nodes
                  btn.setAttribute('show', 'True')
                  btn.setAttribute('species', species_list[i])
                  btn.setAttribute('id', 'show_hide' + "_"+species_list[i]);
-                 btn.addEventListener("click", function(){
-                 if (this.getAttribute('show')=='True')
-                 {console.log("hiding now");
-                 show_hide(this.getAttribute('species'), 'False');
-                 this.setAttribute('show', 'False');
-                 this.style.background = "#ff9263";}
-                 else
-                 {console.log("showing now");
-                 this.setAttribute('show', 'True');
-                 show_hide(this.getAttribute('species'), 'True');
-                 this.style.background = "#91ffd4";};
-                console.log(this.getAttribute('species'));
-                   });
-                 // Fill select option with data:
                  btn.innerHTML = species_list[i];
                  btn.value = species_list[i];
                  btn.style.border="4px solid "+color_node(species_list[i], "")
                  btn.style.background = "#91ffd4";
-                 //btn.style.background=color_node(species_list[i], "");
+                 // On click, change state of button and show or hide nodes for this assembly
+                 btn.addEventListener("click", function(){
+                 // If currently showing nodes, hide them now
+                 if (this.getAttribute('show')=='True')
+                 {
+                 show_hide(this.getAttribute('species'), 'False');
+                 this.setAttribute('show', 'False');
+                 // Give button a red background color
+                 this.style.background = "#ff9263";
+                 }
+                 // If currently hiding nodes, show them now
+                 else
+                 {
+                 this.setAttribute('show', 'True');
+                 show_hide(this.getAttribute('species'), 'True');
+                 // Remove red background from button
+                 this.style.background = "#91ffd4";
+                 };});
+                 // Add button to color legend div
                  colorLegend.appendChild(btn);
              };
-
-//btn.setAttribute('onclick', functions[i]);
-
-
 }
