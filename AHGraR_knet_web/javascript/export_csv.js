cda411c105ce06c82a5c494991176cea1a488ad9
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 15:05:33 +0000

    Export graph data as csv
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1254 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/export_csv.js
+++ AHGraR_knet_web/javascript/export_csv.js
@@ -41,6 +41,10 @@ var protein_nodes_data = {};
 for (i = 0; i < protein_nodes.length; ++i){
 protein_nodes_data[protein_nodes[i].data("id")]=[protein_nodes[i].data("name"),protein_nodes[i].data("species")]
 };
+// Iterate through gene homologs
+for (i = 0; i < homolog_edges.length; ++i){
+console.log(gene_nodes_data[homolog_edges[i]}]);
+}
 //console.log(gene_nodes_data);
 //console.log(protein_nodes_data);
 //console.log(coding_edges);
