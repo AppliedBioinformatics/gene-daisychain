ce5365f422d5927e0922e987cec7a0a02d7e4e9b
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 07:31:14 +0000

    Enable box selection
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1172 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/knet-container.js
+++ AHGraR_knet_web/javascript/knet-container.js
@@ -1,58 +1,29 @@
 function load_reload_Network(network_json, network_style/*, runNetLayout*/) {
 
-//console.log("style: "+ JSON.stringify(network_style, null, 4));
-//console.log("load_reload_Network: elements: "+ JSON.stringify(network_json, null, 4));
-
 // Initialise a cytoscape container instance on the HTML DOM using JQuery.
 $('#cy').cytoscape({
   container: document.getElementById('cy')/*$('#cy')*/,
-
   style: network_style,
-
   // Using the JSON data to create the nodes.
   elements: network_json,
-  
-//  layout: /*defaultNetworkLayout*/ coseNetworkLayout, // layout of the Network
-  // these options hide parts of the graph during interaction such as panning, dragging, etc. to enable faster rendering for larger graphs.
-//  hideLabelsOnViewport: true,
-//  hideEdgesOnViewport: true,
-
   // this is an alternative that uses a bitmap during interaction.
   textureOnViewport: false, // true,
-  /* the colour of the area outside the viewport texture when initOptions.textureOnViewport === true can
-   * be set by: e.g., outside-texture-bg-color: white, */
-
   // interpolate on high density displays instead of increasing resolution.
   pixelRatio: 1,
-
   // Zoom settings
   zoomingEnabled: true, // zooming: both by user and programmatically.
-//  userZoomingEnabled: true, // user-enabled zooming.
   zoom: 1, // the initial zoom level of the graph before the layout is set.
-//  minZoom: 1e-50, maxZoom: 1e50,
-  /* mouse wheel sensitivity settings to enable a more gradual Zooming process. A value between 0 and 1 
-   * reduces the sensitivity (zooms slower) & a value greater than 1 increases the sensitivity. */
   wheelSensitivity: 0.05,
-
   panningEnabled: true, // panning: both by user and programmatically.
-//  userPanningEnabled: true, // user-enabled panning.
-
-  // for Touch-based gestures.
-//  selectionType: (isTouchDevice ? 'additive' : 'single'),
   touchTapThreshold: 8,
   desktopTapThreshold: 4,
   autolock: false,
   autoungrabify: false,
   autounselectify: false,
-
   // a "motion blur" effect that increases perceived performance for little or no cost.
   motionBlur: true,
-
   ready: function() {
-   //if(runNetLayout===true) {
-    //  setDefaultLayout(); // set default Layout
       rerunLayout(); // reset current layout.
-     //}
    window.cy= this;
   }
 });
@@ -60,75 +31,10 @@ $('#cy').cytoscape({
 // Get the cytoscape instance as a Javascript object from JQuery.
 var cy= $('#cy').cytoscape('get'); // now we have a global reference to `cy`
 
-// cy.boxSelectionEnabled(true); // enable box selection (highlight & select multiple elements for moving via mouse click and drag).
-cy.boxSelectionEnabled(false); // to disable box selection & hence allow Panning, i.e., dragging the entire graph.
+cy.boxSelectionEnabled(true); // enable box selection (highlight & select multiple elements for moving via mouse click and drag).
+//cy.boxSelectionEnabled(false); // to disable box selection & hence allow Panning, i.e., dragging the entire graph.
 
-// Set requisite background image for each concept (node) instead of using cytoscapeJS shapes.
-/* cy.nodes().forEach(function( ele ) {
-  var conType= ele.data('conceptType');
-  var imgName= 'Gene'; // default
-  if(conType === "Biological_Process") {
-     imgName= 'Biological_process';
-    }
-  else if(conType === "Cellular_Component") {
-       imgName= 'Cellular_component';
-      }
-  else if(conType === "Gene") {
-       imgName= 'Gene';
-      }
-  else if(conType === "Protein Domain") {
-     imgName= 'Protein_domain';
-    }
-  else if(conType === "Pathway") {
-     imgName= 'Pathway';
-    }
-  else if(conType === "Reaction") {
-     imgName= 'Reaction';
-    }
-  else if(conType === "Publication") {
-     imgName= 'Publication';
-    }
-  else if(conType === "Protein") {
-     imgName= 'Protein';
-    }
-  else if(conType === "Quantitative Trait Locus") {
-     imgName= 'QTL';
-    }
-  else if(conType === "Enzyme") {
-     imgName= 'Enzyme';
-    }
-  else if(conType === "Molecular_Function") {
-     imgName= 'Molecular_function';
-    }
-  else if((conType === "Enzyme_Classification") || (conType === "Enzyme Classification")) {
-     imgName= 'Enzyme_classification';
-    }
-  else if(conType === "Trait Ontology") {
-     imgName= 'Trait_ontology';
-    }
-  else if(conType === "Scaffold") {
-     imgName= 'Scaffold';
-    }
-  else if((conType === "Compound") || (conType === "SNP")) {
-     imgName= 'Compound';
-    }
-  else if(conType === "Phenotype") {
-     imgName= 'Phenotype';
-    }
-  var eleImage= 'image/'+ imgName +'.png';
-//  var eleImage= data_url +'image/'+ imgName +'.png';
 
-  // Add these properties to this element's JSON.
-  ele.data('nodeImage', eleImage);
-//  console.log("data.nodeImage "+ ele.data('nodeImage'));
- });
-
- // Update the stylesheet for the Network Graph to show background images for Nodes.
- cy.style().selector('node').css({ // Show actual background images.
-           'background-image': 'data(nodeImage)',
-           'background-fit': 'none' // can be 'none' (for original size), 'contain' (to fit inside node) or 'cover' (to cover the node).
-          }).update();
-*/
 
 /** Add a Qtip message to all the nodes & edges using QTip displaying their Concept Type & value when a 
  * node/ edge is clicked.
@@ -226,23 +132,8 @@ cy.elements().qtip({
              updateCyLegend();
             }
         },
-
         {
-         content: "Show 5'",
-         select: function()
-            {
-                addPath(this, "5NB");
-            } 
-        },
-        {
-         content: "Show 3'",
-         select: function()
-            {
-                addPath(this, "3NB");
-            } 
-        },
-        {
-         content: "Show 53'",
+         content: "Show 5'/3''",
          select: function()
             {
                 addPath(this, "53NB");
