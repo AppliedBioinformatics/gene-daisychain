ff79757e44a5e8ece2a874bdf7cf788386d14c97
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-03-31 13:56:11 +0000

    Export graph data as csv
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1233 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/export_csv.js
+++ AHGraR_knet_web/javascript/export_csv.js
@@ -3,7 +3,7 @@
 function export_table(){
 console.log("Export table");
 var cy= $('#cy').cytoscape('get'); // now we have a global reference to `cy`
-var gene_nodes = cy.filter(function(i,ele){console.log(ele); return true;});
+var gene_nodes = cy.filter(function(i,ele){console.log(ele); return false;});
 
 console.log(gene_nodes);
 
