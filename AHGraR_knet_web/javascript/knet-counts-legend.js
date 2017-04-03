0a00802a02d7804d4a408197996ba275bcc22ddf
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-04-03 07:31:57 +0000

    Status bar showing hidden node count
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1274 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/knet-counts-legend.js
+++ AHGraR_knet_web/javascript/knet-counts-legend.js
@@ -26,7 +26,7 @@
         };
         
     });
-	var cyLegend= "Showing "+gene_count+" genes and "+protein_count+" proteins";
+	var cyLegend= "Showing "+gene_count+" genes ("+gene_hidden_count" hidden) and "+protein_count+" proteins("+protein_hidden_count+" hidden)";
 
 
 //	console.log(cyLegend);
