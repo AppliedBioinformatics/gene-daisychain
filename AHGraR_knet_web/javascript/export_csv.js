3bf5d878f7b1b188598c0231255060d65cbb229e
Nils-Oliver Schliebs <nils-oliver.schliebs@student.uni-tuebingen.de>
2017-04-04 11:46:32 +0000

    Sort gene node data
    git-svn-id: svn+ssh://magnus/group/pawsey0149/svnrepos/AHGraR@1303 c65c1d0c-9dc2-11e3-8d63-95b8455c46b6

--- AHGraR_knet_web/javascript/export_csv.js
+++ AHGraR_knet_web/javascript/export_csv.js
@@ -102,13 +102,13 @@ hmlg_dict[target] = [source+":"+perc_match];
 csv_file = ["id","name","assembly","contig","start","stop","annotation","homologs"].join(",");
 // Convert each gene_node_data into a row
 // First, convert the gene_node_data object into an array
-gene_nodes_data = $.map(gene_nodes_data, function(key, val){return [val]});
+gene_nodes_data = $.map(gene_nodes_data, function(val, key){return [val]});
 gene_nodes_data.sort(function(a,b){return a.id-b.id});
 console.log(gene_nodes_data);
 for (i = 0; i < gene_nodes_data.length; ++i)
 {
 console.log(gene_nodes_data[i]);
-csv_file += gene_nodes_data[i].join(",");
+//csv_file += gene_nodes_data[i].join(",");
 };
 console.log(csv_file);
 
